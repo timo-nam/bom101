be7be7ce99ac0ff046f8a95edf5236c3bacbb453
Timo Nam <dwbh7358@gmail.com>
2022-12-18 01:43:14 +0900

    generate a Spring Boot project

---
+++ src/test/java/study/bom/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package study.bom.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

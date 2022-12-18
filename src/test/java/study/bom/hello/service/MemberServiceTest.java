3858c13410926035eaf71b948355fca037063c01
Timo Nam <dwbh7358@gmail.com>
2022-12-19 00:53:03 +0900

    refactor: replace try-catch with assertThrows()

--- src/test/java/study/bom/hello/service/MemberServiceTest.java
+++ src/test/java/study/bom/hello/service/MemberServiceTest.java
@@ -41,14 +41,19 @@ class MemberServiceTest {
         Member member2 = new Member();
         member2.setName("Bom");
 
-        // when
+        // when, then
         memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+/*
         try {
             memberService.join(member2);
-        // then
             fail();
         } catch(IllegalStateException e) {
             assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
         }
+*/
     }
 }

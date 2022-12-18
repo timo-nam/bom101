fd4825a7af353d61ecc9477e58c20949c461f2b1
Timo Nam <dwbh7358@gmail.com>
2022-12-18 11:57:16 +0900

    feat: add HelloController with the template

---
+++ src/main/java/study/bom/hello/controller/HelloController.java
@@ -0,0 +1,15 @@
+package study.bom.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String Hello(Model model) {
+        model.addAttribute("data", "Bom");
+        return "hello";
+    }
+}

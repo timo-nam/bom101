d800fbb7d41459378780fcbad3f64ab471799349
Timo Nam <dwbh7358@gmail.com>
2022-12-22 07:03:00 +0900

    feat: add SpringDataJpaMemberRepository and inject it into the service

--- src/main/java/study/bom/hello/SpringConfig.java
+++ src/main/java/study/bom/hello/SpringConfig.java
@@ -3,29 +3,21 @@ package study.bom.hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import study.bom.hello.repository.JpaMemberRepository;
 import study.bom.hello.repository.MemberRepository;
 import study.bom.hello.service.MemberService;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
---
+++ src/main/java/study/bom/hello/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package study.bom.hello.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.bom.hello.domain.Member;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
